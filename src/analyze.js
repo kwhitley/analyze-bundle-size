34b7b43e7c3e0a4f3fec797e32589d6c8731dc85
Kevin Whitley <krwhitley@gmail.com>
2020-02-26 23:27:03 -0600

    working gzip size analyzer

--- src/analyze.js
+++ src/analyze.js
@@ -1,26 +1,91 @@
-const { magenta, green } = require('chalk')
+const { magenta, green, grey, yellow, magentaBright} = require('chalk')
+const filesize = require('filesize')
 const fs = require('fs-extra')
 const globby = require('globby')
-const gzip = require('gzip-size')
+const gzipSize = require('gzip-size')
 const cmd = require('node-cmd')
 const path = require('path')
+const pluralize = require('pluralize')
+const strip = require('strip-color')
 
 const ROOT_PATH = path.resolve()
+const COLUMN_WIDTH = 20
+
+const sortByKey = (key, ascending) => (a, b) => a[key] < b[key] ? (ascending ? -1 : 1) : (a[key] > b[key] ? (ascending ? 1 : -1) : 0)
+
+function displayStats(stats = {}, options = {}) {
+  let {
+    sortBy = 'size',
+    ascending = false,
+    columnWidth = COLUMN_WIDTH,
+    columnWidths = [],
+    showTotal = true,
+  } = options
+
+  let extensions = Object.values(stats).sort(sortByKey(sortBy, ascending))
+
+  extensions.forEach(info => {
+    let { extension, size, count, gzipped } = info
+
+    const columns = [
+      yellow(count) + ' ' + green(`${extension}`) + ' ' + grey(pluralize('file', count)),
+      filesize(size),
+      `${magentaBright(filesize(gzipped))} ${magenta('gzipped')}`,
+    ]
+
+    console.log(...columns.map((c, i) => {
+      const padding = strip(c).padEnd(columnWidths[i] || columnWidth).replace(/^.*?(\s+)$/, '$1')
+      
+      return c + padding
+    }))
+  })
+}
 
 async function analyze() {
-  const files = await globby(ROOT_PATH + '/src`', {
+  console.log(`\nanalyzing ${ROOT_PATH}...\n`)
+
+  const files = await globby(ROOT_PATH, {
     dot: false,
+    gitignore: true,
     onlyFiles: true,
   })
 
-  const extensions = new Map
+  const sizelist = {}
 
   for (var filepath of files) {
-    console.log(filepath)
+    const [ extension ] = filepath.match(/\w+$/) || []
+    
+    if (!extension) continue
+
+    let details = sizelist[extension]
+
+    if (!details) {
+      details = {
+        extension,
+        files: [],
+        count: 0,
+        size: 0,
+        gzipped: 0,
+      }
+    }
+
+
+    const filestats = await fs.stat(filepath)
+    const file = await fs.readFile(filepath)
+    const gzippedSize = await gzipSize(file)
+    
+    details.count++
+    details.files.push(filepath)
+    details.size += filestats.size
+    details.gzipped += gzippedSize
+
+    sizelist[extension] = details
   }
 
-  console.log(JSON.stringify(files, null, 2))
+  displayStats(sizelist, {
+    columnWidths: [22, 15, 20],
+    summarize: ['jpg', 'html', 'js'],
+  })
 }
 
-console.log(magenta(`analyzing: ${ROOT_PATH}`))
 analyze()
